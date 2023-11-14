ea3468d321a21ed0143f89350cdb5fc2e94cb844
Kamil Domian <kamdomian@gmail.com>
2023-11-14 21:49:21 +0100

    Pierwszy commit

---
+++ src/main/java/app/crisis/CrisisApp/CrisisAppApplication.java
@@ -0,0 +1,13 @@
+package app.crisis.CrisisApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrisisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/crisis/CrisisApp/CrisisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.crisis.CrisisApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrisisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
